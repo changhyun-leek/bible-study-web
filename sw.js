63df34ca759329f00cf7526a5450003c52c1da4f
changhyun-leek <changhyun-leek@users.noreply.github.com>
2026-07-31 07:57:45 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bible-study-web-v2';
+const CACHE_NAME = 'bible-study-web-v3';
 const APP_SHELL = ['./', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
